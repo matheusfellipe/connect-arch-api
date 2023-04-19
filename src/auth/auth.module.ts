a0ec088323e10435d8272bcab27b6707a9137dec
Matheus Fellipe <matheus060399@gmail.com>
2023-04-19 11:26:13 -0400

    fix: refresh token use case

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,26 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
 
 import { UsersModule } from '../users/users.module';
 import { authConfig } from './config/auth.cofig';
 import { GenerateAccessAndRefreshTokenUseCase } from './use-cases/generate-token/generate-token-and-refresh.use-case';
 import { AuthUseCase } from './use-cases/login/auth.use-case';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './use-cases/login/auth.controller';
+import { RefreshTokenUseCase } from './use-cases/refresh-token/refresh-token.use-case';
+import { RefreshTokenRepository } from './repositories/refresh-token.repository';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtRefreshTokenStrategy } from './strategies/jwt-refresh-token.strategy';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
+  controllers:[AuthController],
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.register({
       secret: authConfig.secret,
       signOptions: {
@@ -20,6 +31,6 @@ import { AuthUseCase } from './use-cases/login/auth.use-case';
       },
     }),
   ],
-  providers: [AuthUseCase, GenerateAccessAndRefreshTokenUseCase],
+  providers: [AuthUseCase, GenerateAccessAndRefreshTokenUseCase,RefreshTokenUseCase,RefreshTokenRepository,LocalStrategy,JwtStrategy,JwtRefreshTokenStrategy,{provide: APP_GUARD,useClass:JwtAuthGuard}],
 })
 export class AuthModule {}
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
+import { authConfig } from '../config/auth.cofig'; 
+import { PayloadToken } from './interfaces/jwt.interfaces';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: authConfig.secret,
+      jsonWebTokenOptions: {
+        audience: authConfig.audience,
+        issuer: authConfig.issuer,
+      },
+    } as StrategyOptions);
+  }
+
+  async validate(payload: PayloadToken) {
+    return { studentId: payload.sub, userRole: payload.role };
+  }
+}
